f9d3af38388442635c371d2cbd5e755b15e7400e
龔育晴 <ching85117@gmail.com>
2018-01-10 03:21:20 +0800

    後台新增功能選單
    Signed-off-by: 龔育晴 <ching85117@gmail.com>

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.master')
-@section('title','後台')
 @section('content')
-
-
-
+<div class="list-group" style="margin-top: 10%">
+        <a href="#" class="list-group-item list-group-item-action active">後臺管理</a>
+        <a href="#" class="list-group-item list-group-item-action">器材管理</a>
+        <a href="#" class="list-group-item list-group-item-action">申請審核</a>
+        <a href="#" class="list-group-item list-group-item-action">租借記錄</a>
+</div>
 
 @endsection
