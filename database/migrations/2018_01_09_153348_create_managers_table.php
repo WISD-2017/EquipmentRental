0b355b08e875fd453a38a7fbf3364ff7cc64a0eb
陳姿穎 <roeyang1410@gmail.com>
2018-01-09 23:35:41 +0800

    建立managers的migration

---
+++ database/migrations/2018_01_09_153348_create_managers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateManagersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('manager', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('password');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('manager');
+    }
+}
