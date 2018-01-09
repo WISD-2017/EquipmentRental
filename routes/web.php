4aa93f150c8ddff77af93024cd71a2458502ffbe
陳人傑 <h220641963@gmail.com>
2018-01-10 00:53:28 +0800

    建立一個前端頁面 將根路由設置index  修改登入使其正常運作

--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,13 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 Route::get('/admin', function () {
     return view('admin');
 });
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 Auth::routes();
