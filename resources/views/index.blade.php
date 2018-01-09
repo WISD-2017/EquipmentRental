9902fe722a6c24369bcec773316e020d930ab2b3
龔育晴 <ching85117@gmail.com>
2018-01-10 03:22:40 +0800

    前台介面
    Signed-off-by: 龔育晴 <ching85117@gmail.com>

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,26 @@
 @section('title',' ')
 @section('content')
 
-
-
-
+<div style="margin-top: 5%;">
+<center>
+    <table style="font-family: 微軟正黑體">
+        <tr style="border-bottom-color: #888888">
+            <td>
+                器材名稱
+            </td>
+            <td>
+                可用數量
+            </td>
+        </tr>
+        <tr>
+            <td>
+                （器材名稱）
+            </td>
+            <td>
+                （可用數量）
+            </td>
+        </tr>
+    </table>
+</center>
+</div>
 @endsection
