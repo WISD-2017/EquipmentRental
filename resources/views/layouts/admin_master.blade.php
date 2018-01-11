12dbbd47c8a9f1fb0cb3e4ef29c26c121a8900f8
陳人傑 <h220641963@gmail.com>
2018-01-11 21:24:55 +0800

    修正錯誤

--- resources/views/layouts/admin_master.blade.php
+++ resources/views/layouts/admin_master.blade.php
@@ -70,9 +70,9 @@
 </nav>
 <div class="list-group" style="margin-top: 10%">
 
-    <a href="{{url('/admin/manage')}}" class="list-group-item list-group-item-action">器材管理</a>
-    <a href="{{url('/admin/check')}}" class="list-group-item list-group-item-action">申請審核</a>
-    <a href="{{url('/admin/record')}}" class="list-group-item list-group-item-action">租借記錄</a>
+    <a href="{{url('/manage')}}" class="list-group-item list-group-item-action">器材管理</a>
+    <a href="{{url('/check')}}" class="list-group-item list-group-item-action">申請審核</a>
+    <a href="{{url('/record')}}" class="list-group-item list-group-item-action">租借記錄</a>
 </div>
 <!-- Page Content -->
 <div class="container">
