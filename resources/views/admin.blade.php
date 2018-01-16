21ca67614cca55b582501a26bf7abd4975d491d3
陳人傑 <h220641963@gmail.com>
2018-01-16 19:23:39 +0800

    新增後台歡迎介面

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,6 +1,15 @@
 @extends('layouts.admin_master')
 @section('content')
-
+    <br>
+    <center>
+    <table style="font-family: 微軟正黑體; margin-top: 5%">
+        <tr style="border-bottom-color: #888888">
+            <td>
+                歡迎進入後台介面,請選擇左方想查看的介面
+            </td>
+        </tr>
+    </table>
+    </center>>
 
 
 @endsection
