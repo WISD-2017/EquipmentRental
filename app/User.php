2856b0aa9611d60250becd5489f18112fdc9186e
陳人傑 <h220641963@gmail.com>
2018-01-16 19:20:42 +0800

    修改使用者資料表多了學號欄位

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'user_class',
+        'name', 'email', 'password', 'user_class','user_id',
     ];
 
     /**
