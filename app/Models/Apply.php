68441366094fcc5049ac5760f322d21019329a17
陳人傑 <h220641963@gmail.com>
2018-01-17 04:01:22 +0800

    修改Apply 模型

--- app/Models/Apply.php
+++ app/Models/Apply.php
@@ -8,6 +8,6 @@ class Apply extends Model
 {
 protected $table = 'rent_situations';
 protected $fillable=['user_class', 'user_id', 'user_name', 'equipments_name', 'equipments_date',
-                    'equipments_rentalnumber', 'equipments_from', 'equipments_too'];
+                    'equipments_rentalnumber', 'equipments_from', 'equipments_too',];
 
     }
